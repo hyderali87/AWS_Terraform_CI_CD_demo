4a80f0f5bf6a2f04527a837424f5a6ca94cba1f9
hyderali87 <34377320+hyderali87@users.noreply.github.com>
2023-01-23 13:10:14 +0530

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -4,9 +4,9 @@ terraform {
   backend "s3" {
     bucket         = "cc-tf-state-backend-ci-cd"
     key            = "tf-infra/terraform.tfstate" 
-    region = "ap-northeast-1"
-    access_key = "AKIAZ3V3IRELCYUFMGBZ"
-    secret_key = "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"
+    region         = "ap-northeast-1"
+    access_key     = "AKIAZ3V3IRELCYUFMGBZ"
+    secret_key     = "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"
     dynamodb_table = "terraform-state-locking"
     encrypt        = true
   }
@@ -21,7 +21,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-northeast-1"
 }
 
 module "tf-state" {
