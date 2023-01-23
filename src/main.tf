786a0d40d3cb360b23e6d39b9f5ca477c3b9d22f
hyderali87 <34377320+hyderali87@users.noreply.github.com>
2023-01-23 16:13:13 +0530

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -2,7 +2,7 @@ terraform {
   # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
   # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
   backend "s3" {
-    bucket         = "cc-tf-state-backend-ci-cd"
+    bucket         = "terraformtfstatecxup"
     key            = "tf-infra/terraform.tfstate" 
     region         = "us-east-1"
     profile        = "somaf"
